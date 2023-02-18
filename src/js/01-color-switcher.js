8b74ec136441df52633093f333398704a5938e8b
Oksana Fylypchak <107584846+Kseniya1417@users.noreply.github.com>
2023-02-18 20:12:16 +0100

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const dataStart = document.querySelector('[data-start]');
+const dataStop = document.querySelector('[data-stop]');
+const bodyBackground = document.querySelector('body');
 
+dataStart.addEventListener('click', btnStart);
+dataStop.addEventListener('click', btnStop);
+
+let timerId = null;
+dataStop.disabled = true;
+
+function btnStart() {
+  timerId = setInterval(getBackgroundColor, 1000);
+  dataStart.disabled = true;
+  dataStop.disabled = false;
+}
+
+function btnStop() {
+  clearInterval(timerId);
+
+  dataStart.disabled = false;
+  dataStop.disabled = true;
+}
+
+function getBackgroundColor() {
+  bodyBackground.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
