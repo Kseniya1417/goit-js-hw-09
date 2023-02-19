60b15039b809bfc8825f375505286d84f61a8f1c
Oksana Fylypchak <107584846+Kseniya1417@users.noreply.github.com>
2023-02-19 16:04:12 +0100

    took away the comments

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,96 +1,52 @@
-// npm instal
-// додати import
-// ініціалізували, тобто використали (у нас на input)
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-//готуємо місце input, де ми будемо ініціалізувати нашу бібліотеку
 const input = document.querySelector('#datetime-picker');
 const days = document.querySelector('[data-days]');
 const hours = document.querySelector('[data-hours]');
 const minutes = document.querySelector('[data-minutes]');
 const seconds = document.querySelector('[data-seconds]');
 const btnStart = document.querySelector('[data-start]');
-// пишемо ці змінні пока функцією щоб мати до них доступ з будь-якого місця в коді
-// (до поточної дати, до обраної дати) бо вони нам будуть треба в інших функціях
-// записуємо їх в let бо вони будуть змінюватись
+
 let selectedDate = null;
 let currentDate = null;
 let timerId = null;
 
-// з умови задачі взято: "Кнопка «Start» повинна бути неактивною доти, доки користувач не вибрав дату в майбутньому."
 btnStart.disabled = true;
 
-// беремо з умови задачі константу options
-// onClose це стандартний метод нашої бібліотеки, він просто існує і ми маєм його якось модифікувати.
-// Він підключається під час закриття елемента, звідси і його назва. Елемент - це календарик, який зявляється
-// при кліку на input
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  // onClose - це стандартний метод нашої бібліотеки він просто існує і ми маємо його якось модифікувати
-  // параметр selectedDates - це масив дат
-  // в консолі буде дата яку ви вибрали, тому і називається selectedDates (обрані дати -
-  // можна вибирати період дат, тому тут масив, але він як масив нам зараз не потрібен)
-  // нульовий, тобто перший елемент цього масиву - це наша обрана дата
   onClose(selectedDates) {
-    console.log(selectedDates[0]); // як ця дата сюди потрапляє нас не цікавить, це робить бібліотека, але ми маємо доступ
+    console.log(selectedDates[0]);
     selectedDate = selectedDates[0];
-    currentDate = new Date(); // в currentDate ми записуєм сьогоднішню дату за допомогою екземпляра класу new date
-    // далі магія: як ми мажм визначити що наа дата в минулому
-    // тепер ми маємо порівняти обрану дату з поточною датою якщо обрана дата (selectedDates) більша то
-    // це значить що що вона в майбутньому
+    currentDate = new Date();
     if (selectedDate > currentDate) {
       btnStart.disabled = false;
-    }
-    // а якщо інакше то це минуле і ми показуюєм користувачу повідомлення з використанням бібліотеки, а можна ще alert
-    // юзер попадає після того як знову вибрав, але вже правильну дату на рядок 44 і тоді кнопка стає активною
-    else {
+    } else {
       Notify.warning('Please choose a date in the future');
     }
   },
 };
 
-// запускаєм flatpiker (бібліотека), взяли з умови задачі
 flatpickr(input, options);
 
-// вішаємо слухача на кнопку
 btnStart.addEventListener('click', startTimer);
 
 function startTimer() {
   btnStart.disabled = true;
-  // для того щоб функція раз на секунду це змінювала ми використовуємо setInterval
   timerId = setInterval(() => {
-    // сurrentDate краще задати ще раз бо може бути глюк, з 40 стрічки може не потягнути
     currentDate = new Date();
-    //  для того щоб запустити таймер нам потрібно знайти різницю між оцими датами
-    // потрібні константи зберігаються за межами тому тут ми маємо до них доступ
-    // Різниця нам потрібна для того щоб знати на скільки (на яку різницю) нам запустити таймер
-    // якщо різниця 1 день то таймер запуститься на 24 години, якщо більше - то на інше число
     const differenceTime = selectedDate.getTime() - currentDate.getTime();
-    // треба зупиняти таймер яко він дійшов до нуля, ми ставимо перевірку (if)
     if (differenceTime > 0) {
-      // ми маємо цю різницю differenceTime передати в функцію onvertMs
-      // ця різниця передається у 87й рядок і ця функція нам допоможе
-      // з цієї різниці виділити ці дні, години, хвилини і секунди
-      // результат (return) функції convertMs ми отримуєм у наступному рядку
-      // і зробили для нього константу
       const convertedTime = convertMs(differenceTime);
-      // тепер треба заповнити таймер тими цифрами які нам прийшли
-      // ми беремо змінні, які завбачливо підготували спочатку
-      // використовуємо innerHTML щоб переписати вміст
-      // спочатку функцію не викликаємо і з convertedTime беремо дні і записуємо в дні і т.д
-      // коли ми згадуємо що внизу десь є функція яка дописує знаки, якщо їх менше 10
-      // тому ми її тут викликаєм addZero. addZero - це функція, яка просто форматує
       days.innerHTML = addZero(convertedTime.days);
       hours.innerHTML = addZero(convertedTime.hours);
       minutes.innerHTML = addZero(convertedTime.minutes);
       seconds.innerHTML = addZero(convertedTime.seconds);
-      // якщо різниця дат менша нуля то ми зупиняємо таймер, тоді ми дописуємо перед функцією
-      // setInterval:timerId і пишемо вище його початкове значення ставимо null
     } else {
       clearInterval(timerId);
     }
@@ -98,29 +54,17 @@ function startTimer() {
 }
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
-
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
   return { days, hours, minutes, seconds };
 }
 
 function addZero(value) {
-  // працює тільки з рядками, він заповнює рядок, якщо символів буде менше 2х то він допише спереду 1 нуль
-  // функція addZero приймає значення, сюди у значення (value) прийде число бо функція convertMs повертає числа
-  //  тому у (value) прийде число, а padstart працює з рядками тому ми маємо перетворити примусово те що ми
-  // отримали value на string. Не треба ніяких if бо padstar сам розуміє що якщо символів 2 (наш перший аргумент)
-  // і нема місця де втулити 0 він не втулить. Обов'язково треба return щоб він нам щось повернув
   return String(value).padStart(2);
 }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,21 +5,11 @@ const firstDelay = document.querySelector('[name="delay"]');
 const stepDelay = document.querySelector('[name="step"]');
 const amountInput = document.querySelector('[name="amount"]');
 
-// у цій функції проміс створюється 1 раз,
-// один єдиний проміс, який називається new Promise, але з затримкою
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  // ми маємо створити проміс, подивились по конспекту що проміс створюється отак:
-  // return має бути бо ця функція має нам щось повертати
   return new Promise((resolve, reject) => {
-    // timeout це затримка, для кожного промісу вона буде своя
     setTimeout(() => {
-      // у timeout ставимо if і його заповнюєм
       if (shouldResolve) {
-        // якщо у нас проміс resolve то ми як результат повертаєм
-        // position - це номер промісу і delay - це затримка
-        // бо так є в умові що значенням promis має бути об'єкт
-        // в якому будуть властивості position, delay
         resolve({ position, delay });
       } else {
         reject({ position, delay });
@@ -27,42 +17,15 @@ function createPromise(position, delay) {
     }, delay);
   });
 }
-form.addEventListener('submit', onFormSubmit);
 
-// функція приймає event бо ми працюєм з подією
+form.addEventListener('submit', onFormSubmit);
 function onFormSubmit(event) {
-  // стандартна поведінка браузера при submit перегрузити сторінку
-  // нам це не потрібно тому ставим
   event.preventDefault();
-  // щоб робити щось із значеннями input ми повинні їх спочатку повитягувати
-  // переробляємо в Number бо з input приходить string
-  // робимо число з першого, другого і третього inputa
   let delay = Number(firstDelay.value);
   const step = Number(stepDelay.value);
   const amount = Number(amountInput.value);
-  // нам потрібно зробити стільки промісів скільки написано у значенні input amount
-  // тобто amount разів
-  // потрібна дія, яка виконується багато разів
-  // якщо у нас однотипна дія, яка виконується багато разів
-  // значить нам треба цикл
-  // але тут немає ні об'єкти ні масива щоб використати цикли forEach
-  // тому тут підходить простенький for
-  // у цьому циклі має викликатися функція createPromise, яка створює проміс
-  // нам треба передати у цю функцію затримку і номер промісу
-  // номер промісу ми можемо взяти з ітерацій циклу через for
-  // на першому циклі ми створимо перший проміс, на другому - другий і т.д
-  // номер промісу ми можемо взяти з "і"
   for (let i = 1; i <= amount; i += 1) {
-    // в цьому циклі викликаємо іункцію яка створює проміси, вона є готова у завданні
-    // ми створили проміс, після того як він або resolve або reject
-    // ми попадаємо у then
     createPromise(i, delay)
-      // then як ланцюжок будується - після того як ми створили проміс
-      // і він відпрацював
-      // then приймає position, delay
-      // і виводить оце повідомлення за допомогою бібліотеки
-      // це результат оброблення промісу, після промісу у нас завжди буде then або catch
-      // якщо позитивний результат то then якщо негативний тоc catch
       .then(({ position, delay }) => {
         Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
       })
